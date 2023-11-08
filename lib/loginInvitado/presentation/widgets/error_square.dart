a6a99be4594d833c9d428d461cc1ebfad4838107
Oriana Toubia <ovtoubia.20@est.ucab.edu.ve>
2023-11-08 14:58:46 -0400

    Agregue el login, probe y funciona (sin navegacion)

---
+++ lib/loginInvitado/presentation/widgets/error_square.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ErrorSquare extends StatefulWidget {
+
+
+  const ErrorSquare({super.key});
+
+  @override
+  State<ErrorSquare> createState() => ErrorSquareState();
+}
+
+class ErrorSquareState extends State<ErrorSquare> {
+
+  bool invalidData=false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+                  visible: invalidData,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                    height: 70.0,
+                    width: 428.0,
+                    color: Colors.transparent,
+                    child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width:2.0,),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(Radius.circular(15.0))),
+                      
+                      child:  const Align(
+                        alignment: Alignment.centerLeft,
+                        child:  Padding(
+                          padding:  EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.error, color: Colors.red,),
+                              Text("  Datos inválidos, intenta nuevamente", 
+                                style: TextStyle(color: Colors.black, fontSize: 16),
+                                textAlign: TextAlign.left,),
+                            ],
+                          )
+                        ),
+                      )),
+                    ),
+                    ),
+                    );
+
+  }
+}
